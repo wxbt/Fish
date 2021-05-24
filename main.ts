41b4352cf9b3afe76e0e4a04f9fe8794d5e58ac8
wxbt <50980203+wxbt@users.noreply.github.com>
2021-05-24 23:47:21 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 
-//% color="#D2691E" weight=10 icon="\uf1b9" block="鱼与熊掌小车"
+//% color="#D2691E" weight=20 icon="\uf1b9" block="鱼与熊掌小车"
 namespace fish {
     const PCA9685_ADDRESS = 0x40
     const MODE1 = 0x00
